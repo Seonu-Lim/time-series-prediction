9a32d6a800f843f7201c03ef46a7dd25f68b5927
seonu <2013101066sw@gmail.com>
2020-11-13 22:35:13 +0900

    edit _test.py

---
+++ _test.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+import pandas as pd
+
+
+import torch
+import numpy as np
+import pandas as pd
+
+class Prediction(object) :
+
+    def __init__(self,X,param_dict,n_f,seq_length,y_len) :
+
+        self.X = X
+        self.param_dict = param_dict
+        self.n_f = n_f
+        self.seq_length = seq_length
+        self.y_len = y_len
+        self.names = ['ckpt_'+ str(i) for i in range(1,11)]
+
+
+    def get_forecast(self,scaler,start_date) :
+        with torch.no_grad() :
+            testdata = self.X[-1:]
+            forecast = []
+            for j in range(len(self.names)) :
+                y_t_p = self.param_dict[self.names[j]](testdata).cpu().numpy()
+                forecast.append(y_t_p)
+        forecast = [sum(x)/len(x) for x in zip(*forecast)]
+        return forecast
+
+
+    def get_prediction(self,days) :
+        j = days - 1
+        with torch.no_grad() :
+            allpreds = []
+            for j in range(len(self.names)) :
+                preds = []
+                for i in range(len(self.X)):
+                    testdata = self.X[i:i + 1]
+                    y_test_pred = self.param_dict[self.names[j]](testdata)
+                    pred = y_test_pred[:, j].cpu().numpy()
+                    preds.append(pred[0])
+                allpreds.append(preds)
+        preds = [sum(x)/len(x) for x in zip(*allpreds)]
+        return preds
