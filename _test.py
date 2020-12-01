8b6a06619cc7ca584a9ded30c1359e2d1af41e75
itholic <haejoon309@naver.com>
2020-12-01 16:03:03 +0900

    [MINOR] cleanup code for respecting PEP8

--- _test.py
+++ _test.py
@@ -1,10 +1,8 @@
 import torch
-import numpy as np
-import pandas as pd
 
-class Prediction(object) :
 
-    def __init__(self,X,param_dict,n_f,seq_length,y_len) :
+class Prediction(object):
+    def __init__(self, X, param_dict, n_f, seq_length, y_len):
 
         self.X = X
         self.param_dict = param_dict
@@ -13,31 +11,28 @@ class Prediction(object) :
         self.y_len = y_len
         self.names = list(param_dict.keys())
 
-
-    def get_forecast(self) :
-        with torch.no_grad() :
+    def get_forecast(self):
+        with torch.no_grad():
             testdata = self.X[-1:]
             forecast = []
-            for j in range(len(self.names)) :
+            for j in range(len(self.names)):
                 y_t_p = self.param_dict[self.names[j]](testdata).cpu().numpy()
                 forecast.append(y_t_p)
-        forecast = [sum(x)/len(x) for x in zip(*forecast)]
+        forecast = [sum(x) / len(x) for x in zip(*forecast)]
         forecast = forecast[0].tolist()
         return forecast
 
-
-
-    def get_prediction(self,days) :
+    def get_prediction(self, days):
         j = days - 1
-        with torch.no_grad() :
+        with torch.no_grad():
             allpreds = []
-            for j in range(len(self.names)) :
+            for j in range(len(self.names)):
                 preds = []
                 for i in range(len(self.X)):
-                    testdata = self.X[i:i + 1]
+                    testdata = self.X[i : i + 1]
                     y_test_pred = self.param_dict[self.names[j]](testdata)
                     pred = y_test_pred[:, j].cpu().numpy()
                     preds.append(pred[0])
                 allpreds.append(preds)
-        preds = [sum(x)/len(x) for x in zip(*allpreds)]
+        preds = [sum(x) / len(x) for x in zip(*allpreds)]
         return preds
--- _train.py
+++ _train.py
@@ -1,63 +1,72 @@
-from model import (LSTM, GRU, B_LSTM, BiLSTM)
+from model import LSTM, GRU, B_LSTM, BiLSTM
 import numpy as np
 import torch
-from torch import nn, optim
-from torch.utils.data import (Dataset, DataLoader, TensorDataset)
+from torch.utils.data import DataLoader, TensorDataset
 
-def train_model(num_epochs,X,y,path,model_name = 'LSTM'):
+
+def train_model(num_epochs, X, y, path, model_name="LSTM"):
 
     n_f = X.shape[2]
     seq_length = X.shape[1]
     y_len = y.shape[1]
 
-    X_split = torch.split(X,X.shape[0]//10 + 1)
-    y_split = torch.split(y,y.shape[0]//10 + 1)
-    train_hist = np.zeros((num_epochs,(len(X_split))))
-    test_hist = np.zeros((num_epochs,(len(X_split))))
-    for i in range(len(X_split)) :
-        if model_name == 'LSTM' :
+    X_split = torch.split(X, X.shape[0] // 10 + 1)
+    y_split = torch.split(y, y.shape[0] // 10 + 1)
+    train_hist = np.zeros((num_epochs, (len(X_split))))
+    test_hist = np.zeros((num_epochs, (len(X_split))))
+    for i in range(len(X_split)):
+        if model_name == "LSTM":
             model = LSTM(n_features=n_f, n_hidden=128, seq_len=seq_length, y_length=y_len)
-        elif model_name == 'GRU' :
+        elif model_name == "GRU":
             model = GRU(n_features=n_f, n_hidden=128, seq_len=seq_length, y_length=y_len)
-        elif model_name == 'B_LSTM' :
+        elif model_name == "B_LSTM":
             model = B_LSTM(n_features=n_f, n_hidden=128, seq_len=seq_length, y_length=y_len)
-        elif model_name == 'BiLSTM' :
+        elif model_name == "BiLSTM":
             model = BiLSTM(n_features=n_f, n_hidden=128, seq_len=seq_length, y_length=y_len)
         optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        loss_fn = torch.nn.MSELoss() ######## TODO : apply custom loss #########
-        model = model.to('cuda')
-        X_test = X_split[i].to('cuda')
-        y_test = y_split[i].to('cuda')
-        idx = [k for k in range(len(X_split)-1) if k != i]
+        loss_fn = torch.nn.MSELoss()  # TODO : apply custom loss #########
+        model = model.to("cuda")
+        X_test = X_split[i].to("cuda")
+        y_test = y_split[i].to("cuda")
+        idx = [k for k in range(len(X_split) - 1) if k != i]
         X_train = torch.cat([X_split[x] for x in idx])
         y_train = torch.cat([y_split[x] for x in idx])
-        dataset = TensorDataset(X_train,y_train)
+        dataset = TensorDataset(X_train, y_train)
         dataloader = DataLoader(dataset, batch_size=64, shuffle=True, drop_last=False)
-        print(str(i+1)+'th fold training start')
-        for t in range(num_epochs): # epochs per fold
+        print(str(i + 1) + "th fold training start")
+        for t in range(num_epochs):  # epochs per fold
             for batch_idx, samples in enumerate(dataloader):
                 X_train, y_train = samples
-                X_train = X_train.to('cuda')
-                y_train = y_train.to('cuda')
+                X_train = X_train.to("cuda")
+                y_train = y_train.to("cuda")
                 y_pred = model(X_train)
                 loss = torch.sqrt(loss_fn(y_pred.float(), y_train))
                 with torch.no_grad():
                     y_test_pred = model(X_test)
-                    test_loss = torch.sqrt(loss_fn(y_test_pred.float(), y_test)) ######## TODO : apply custom loss #########
-                test_hist[t,i] += test_loss.item()
-                train_hist[t,i] += loss.item()
+                    test_loss = torch.sqrt(
+                        loss_fn(y_test_pred.float(), y_test)
+                    )  # TODO : apply custom loss #########
+                test_hist[t, i] += test_loss.item()
+                train_hist[t, i] += loss.item()
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-            train_hist[t,i] /= len(dataloader)
-            test_hist[t,i] /= len(dataloader)
-            print('Epoch {:3d} train loss: {:.4f} test loss: {:.4f}'.format(t + 1, train_hist[t,i], test_hist[t,i]))
-        print("saving checkpoint at fold", i+1)
-        torch.save({
-                'epoch': num_epochs,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'loss': loss,
-                'test_loss':test_hist[num_epochs-1,i]
-            }, f"model/{path}/ckpt_{i+1}.pt")
-    print('Training Done.')
+            train_hist[t, i] /= len(dataloader)
+            test_hist[t, i] /= len(dataloader)
+            print(
+                "Epoch {:3d} train loss: {:.4f} test loss: {:.4f}".format(
+                    t + 1, train_hist[t, i], test_hist[t, i]
+                )
+            )
+        print("saving checkpoint at fold", i + 1)
+        torch.save(
+            {
+                "epoch": num_epochs,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": loss,
+                "test_loss": test_hist[num_epochs - 1, i],
+            },
+            f"model/{path}/ckpt_{i+1}.pt",
+        )
+    print("Training Done.")
