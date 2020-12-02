6424d7ef6df1eae373dee42d8e6125ed2d775aa5
seonu <2013101066sw@gmail.com>
2020-12-02 23:00:29 +0900

    update class TimeDataSet
    generalization to any time series data

--- util.py
+++ util.py
@@ -66,26 +66,11 @@ class Util:
         return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
-class AgriDataSet(object):
-    def __init__(self, price_path, weather_path):
+class TimeDataSet(object):
+    def __init__(self, price):
+        self.price = price
 
-        self.price_path = price_path
-        self.weather_path = weather_path
-
-        price = pd.read_csv(self.price_path, parse_dates=True, header=None)
-        temp = price.groupby(2).count()[0]
-        k = temp[temp == max(temp)].index[0]
-
-        self.price = (
-            price[price[2] == k]
-            .groupby([0, 1])
-            .mean()
-            .pivot_table(index=0, columns=1, values=3)
-            .mean(axis=1)
-        )
-        self.weather = pd.read_csv(self.weather_path, parse_dates=True, index_col=0)
-
-    def _make_pr_dr_var(self, period, list_of_roll):
+    def make_dataset(self, period, list_of_roll):
 
         price_decomp = seasonal_decompose(
             self.price, model="additive", period=period, extrapolate_trend="freq"
@@ -98,10 +83,3 @@ class AgriDataSet(object):
         price = pd.concat(l_df, axis=1)
 
         return price, price_decomp.seasonal
-
-    def make_agri_dataset(self, period, list_of_roll):
-
-        price, seasonal = self._make_pr_dr_var(period, list_of_roll)
-        df = pd.merge(price, self.weather, left_index=True, right_index=True, how="left")
-
-        return df, seasonal
