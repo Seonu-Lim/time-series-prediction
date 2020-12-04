fbaad96a64e0f6ec22f10d02d4ce88445a3da2a1
seonu <2013101066sw@gmail.com>
2020-12-04 15:34:35 +0900

    Update _loss.py
    user can now choose weights manually for custom loss functions

--- _loss.py
+++ _loss.py
@@ -1,27 +1,48 @@
 import torch
 
-def custom_loss_1(output, target):
+class CustomLoss(object) :
 
-    #### base point is the first value of target variable ####
+    #### user can custom weight ####
 
-    outsgn = torch.sign(output - target[0]*output.shape[1])
-    trgsgn = torch.sign(target - target[0]*output.shape[1])
-    binary = torch.add((outsgn != trgsgn),1)
-    se = (output - target)**2
-    loss = torch.mean(se * binary)
-    
-    return loss
+    def __init__(self,w1,w2) :
 
+        self.w1 = w1
+        self.w2 = w2
+        self.eps = 1e-7
 
-def custom_loss_2(output, target) :
+    def custom_loss_1(self, output, target):
 
-    #### base point is the previous value of target variable ####
+        ### baseline is the first value of target variable ###
 
-    zero = torch.zeros(1,output.shape[1]).to('cuda')
-    binary = torch.sign(output[1:] - target[:-1]) != torch.sign(target[1:]- target[:-1])
-    binary = torch.cat((zero,binary))
-    binary = torch.add(binary,1)
-    se = (output - target) ** 2
-    loss = torch.mean(se * binary)
+        outsgn = torch.sign(output - target[0]*output.shape[1])
+        trgsgn = torch.sign(target - target[0]*output.shape[1])
+        binary = (outsgn != trgsgn)
 
-    return loss
+        w1 = torch.ones(*binary.shape) * self.w1
+        w2 = torch.ones(*binary.shape) * self.w2
+        w1 = w1.to('cuda')
+        w2 = w2.to('cuda')
+
+        binary = torch.where( binary < self.eps, w1, w2)
+        se = (output - target)**2
+        loss = torch.mean(se * binary)
+
+        return loss
+
+    def custom_loss_2(self,output,target):
+
+        ### baseline is the previous value of target variable ###
+
+        zero = torch.zeros(1,output.shape[1]).to('cuda')
+        binary = torch.sign(output[1:] - target[:-1]) != torch.sign(target[1:]- target[:-1])
+        binary = torch.cat((zero,binary))
+        w1 = torch.ones(*binary.shape) * self.w1
+        w2 = torch.ones(*binary.shape) * self.w2
+        w1 = w1.to('cuda')
+        w2 = w2.to('cuda')
+
+        binary = torch.where( binary < self.eps, w1, w2)
+        se = (output - target) ** 2
+        loss = torch.mean(se * binary)
+
+        return loss
